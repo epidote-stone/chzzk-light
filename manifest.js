ba94741d6595b8c56232a2f8cabb6a90cffe5fc6
epidote-stone <epidotestone@gmail.com>
2024-02-04 05:03:39 +0000

    🔧 config: Remove unnecessary permissions from manifest

--- manifest.js
+++ manifest.js
@@ -11,21 +11,9 @@ const manifest = {
   name: '__MSG_extensionName__',
   version: packageJson.version,
   description: '__MSG_extensionDescription__',
-  permissions: ['storage', 'sidePanel'],
-  side_panel: {
-    default_path: 'src/pages/sidepanel/index.html',
-  },
-  options_page: 'src/pages/options/index.html',
   background: {
     service_worker: 'src/pages/background/index.js',
     type: 'module',
-  },
-  action: {
-    default_popup: 'src/pages/popup/index.html',
-    default_icon: 'icon48.png',
-  },
-  chrome_url_overrides: {
-    newtab: 'src/pages/newtab/index.html',
   },
   icons: {
     128: 'icon128.png',
@@ -39,10 +27,9 @@ const manifest = {
     },
     {
       matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['src/pages/contentUI/index.js'],
+      js: ['src/pages/content/injected/index.ts'],
     },
   ],
-  devtools_page: 'src/pages/devtools/index.html',
   web_accessible_resources: [
     {
       resources: ['assets/js/*.js', 'assets/css/*.css', 'icon128.png', 'icon48.png'],
