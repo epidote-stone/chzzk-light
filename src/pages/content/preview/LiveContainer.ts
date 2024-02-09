f954a2d80e5ca9a92e1c62eec875bffe4f4c0768
epidote-stone <epidotestone@gmail.com>
2024-02-09 14:29:32 +0000

    ✨ feat: Update preview content feature
    - Store data in storage
    - Remove duplicate data queries

--- src/pages/content/preview/LiveContainer.ts
+++ src/pages/content/preview/LiveContainer.ts
@@ -2,9 +2,14 @@ class LiveContainer {
   state: { data: { time: Date, id: string, url: string }[] };
 
   constructor() {
-    this.state = {
-      data: []
-    };
+    // localStorage에서 데이터를 로드하고, 없으면 기본값 사용
+    const storedData = localStorage.getItem('liveData');
+
+    // 데이터를 로드할 때 time 속성을 Date 객체로 변환
+    this.state = storedData ? JSON.parse(storedData, (key, value) => {
+      if (key === 'time') { return new Date(value); }
+      return value;
+    }) : { data: [] };
   }
 
   addData(newData: { time: Date, id: string, url: string }) {
@@ -31,17 +36,42 @@ class LiveContainer {
     }
   }
 
+  isDataValid(id: string): boolean {
+    // 데이터 조회
+    const data = this.state.data.find(item => item.id === id);
+
+    // 데이터가 없다면 false 반환
+    if ( !data ) { return false; }
+
+    // 현재 시간 조회
+    const currentTime = new Date();
+
+    // 새로운 데이터의 시간과 현재 시간 사이의 차이를 계산
+    const timeDifference = Math.abs(currentTime.getTime() - data.time.getTime());
+    // 차이가 2분 이내인지 확인
+    const twoMinutesInMilliseconds = 2 * 60 * 1000; // 2분을 밀리초로 변환
+    return timeDifference <= twoMinutesInMilliseconds;
+  }
+
   getDataById(id: string) {
     return this.state.data.find(item => item.id === id);
   }
 
   deleteData(id: string) {
     const index = this.state.data.findIndex(item => item.id === id);
-    if (index !== -1) {
-      this.state.data.splice(index, 1);
-    }
+    if (index !== -1) { this.state.data.splice(index, 1); }
+  }
+
+  // storage에 데이터를 저장하는 메서드
+  saveDataToStorage() {
+    localStorage.setItem('liveData', JSON.stringify(this.state));
+  }
+
+  // 스토리지 데이터, 클래스 state 초기화 메서드
+  clearStorage() {
+    localStorage.removeItem('liveData');
+    this.state = { data: [] };
   }
 }
-
 
 export default LiveContainer;
--- src/pages/content/preview/app.tsx
+++ src/pages/content/preview/app.tsx
@@ -2,168 +2,111 @@ import { useEffect, useState } from 'react';
 import LiveContainer from './LiveContainer';
 
 export default function App() {
-  async function getLiveDetail(liveID) {
-    const response = await fetch(`https://api.chzzk.naver.com/service/v2/channels/${liveID}/live-detail`)
-    const result   = await response.json()
-    const imageURL = result.content.liveImageUrl.replace('{type}', '480')
-    return imageURL
-  }
-
-  function infoElementController(url) {
+  async function infoElementController(hoverID, url) {
     // 라이브 정보 엘리멘트 조회
-    const infoElement = document.querySelector('.navigator_inner__fR6Zm');
-    
-    if (!infoElement || infoElement.querySelector('img')) { return }
+    const infoElementLoadClass = `a[href*="${hoverID}"] .navigator_inner__fR6Zm .navigator_text__MHoCv`;
+    const elementChecker = await checkElementExistence(infoElementLoadClass, 3);
 
-    // 이미지 엘리멘트 생성
-    const infoPreviewElement = document.createElement('img');
-    infoPreviewElement.setAttribute('src', url);
-    infoPreviewElement.setAttribute('alt', 'Preview Image');
-    infoPreviewElement.classList.add('previewImage')
-    infoPreviewElement.style.width = '176px'
+    if (!elementChecker) { return; }
+    const infoElement = document.querySelector(`a[href*="${hoverID}"] .navigator_inner__fR6Zm`);
+    const infoImageElement = infoElement.querySelector('img');
 
-    // 부모 엘리멘트에 이미지 추가
-    infoElement.appendChild(infoPreviewElement);
+    if (!infoImageElement) {
+      // 이미지 엘리멘트 생성
+      const infoPreviewElement = document.createElement('img');
+      infoPreviewElement.setAttribute('src', url);
+      infoPreviewElement.setAttribute('alt', 'Preview Image');
+      infoPreviewElement.classList.add('previewImage')
+      infoPreviewElement.style.width = '176px'
+
+      // 부모 엘리멘트에 이미지 추가
+      infoElement.appendChild(infoPreviewElement);
+      return;
+    }
+    if (!(infoElement instanceof HTMLAnchorElement)) { return; }
+    if (infoElement.href == url) { return; }
+
+    infoElement.href = url;
   }
   
+
+  // 엘리멘트 조회
+  async function checkElementExistence(query: string, retries: number): Promise<Element | null> {
+    let count = 0;
+
+    return new Promise<Element | null>((resolve) => {
+      const intervalId = setInterval(() => {
+        // 요소 선택
+        const element = document.querySelector(query);
+
+        // 요소 존재
+        if (element) {
+          clearInterval(intervalId); // 인터벌 중지
+          resolve(element);          // 선택 요소 반환
+        } else {
+          count++;
+
+          // 지정된 횟수만큼 시도한 경우
+          if (count >= retries) {
+            clearInterval(intervalId); // 인터벌 중지
+            resolve(null);             // 요소 조회 불가 시 null
+          }
+        }
+      }, 100); // 0.1초마다 시도
+    });
+  }
+
+  async function getLiveDetail(liveID) {
+    const response = await fetch(`https://api.chzzk.naver.com/service/v2/channels/${liveID}/live-detail`);
+    const result = await response.json();
+
+    // 성인 방송 썸네일
+    let imageURL = 'https://ssl.pstatic.net/static/nng/glive/resource/p/static/media/image_age_restriction_search.ba30cccc6b1fd2dbf431.png';
+
+    // 성인 방송이 아닐경우
+    if (!result.content.userAdultStatus) { imageURL = result.content.liveImageUrl.replace('{type}', '480'); }
+
+    return imageURL;
+  }
+
 
   useEffect(() => {
     // 라이브 데이터 모음
     const container = new LiveContainer();
 
-    container.addData({ time: new Date(), id: 'uniqueId', url: 'example.com' });
-
-    container.addData({ time: new Date(), id: 'uniqueId', url: 'saaaaa.com' });
-
-    console.log(container.state.data)
-
-    console.log(container.getDataById('uniqueId'))
-
-
     // 채널 탭에 마우스 호버 시
     document.addEventListener('mouseover', async function (event) {
       // 채널 메뉴 호버 유무 확인
-      const hoverElementSelector = '.navigator_item__qXlq9'
-      const targetElement = event.target as HTMLElement
-      
+      const hoverElementSelector = '.navigator_item__qXlq9';
+      const targetElement = event.target as HTMLElement;
+
       // 엘리멘트 조회 여부
-      if (!targetElement || typeof targetElement.matches !== 'function') { return }
-      const closestHoverElement = targetElement.closest(hoverElementSelector)
-      if (!closestHoverElement || !(closestHoverElement instanceof HTMLAnchorElement)) { return }
-      if (!closestHoverElement.href.includes('/live/')) { return }
+      if (!targetElement || typeof targetElement.matches !== 'function') { return; }
+      const closestHoverElement = targetElement.closest(hoverElementSelector);
+      if (!closestHoverElement || !(closestHoverElement instanceof HTMLAnchorElement)) { return; }
+      if (!closestHoverElement.href.includes('/live/')) { return; }
 
       // 호버한 방송 데이터 출력
-      const hoverID  = closestHoverElement.href.replace('https://chzzk.naver.com/live/', '')
-      // container.addData(hover)
-      const hoverURL = await getLiveDetail(hoverID)
+      const hoverID = closestHoverElement.href.replace('https://chzzk.naver.com/live/', '');
 
-      infoElementController(hoverURL)
+      // 조회한지 2분 이상
+      if (!(container.isDataValid(hoverID))) {
+        // 로딩 이미지
+        // 176 99.031
+        // infoElementController()
 
-      /* 
-      const liveInfo = document.querySelector('.navigator_inner__fR6Zm');
-      if (liveInfo && !liveInfo.querySelector('img')) {
-        if (getInquiryStatus(nowHoverLive, liveData)) {
-          const response = await getStreamData(lastHoverLive)
-          const image = response.content.liveImageUrl.replace('{type}', '480')
+        // url 업데이트
+        const hoverURL = await getLiveDetail(hoverID);
+        container.addData({ time: new Date(), id: hoverID, url: hoverURL });
 
-          const previewImage = document.createElement('img');
-          previewImage.setAttribute('src', image);
-          previewImage.setAttribute('alt', 'Image Description');
-          previewImage.classList.add('previewImage')
-          previewImage.style.width = '176px'
-
-          // 이미지를 부모 엘리먼트에 추가
-          liveInfo.appendChild(previewImage);
-        }
+        // 데이터 저장
+        // DataStorage.saveData('liveData', container.state.data);
+        container.saveDataToStorage();
       }
 
-
-      // Element가 
-      if (targetElement && typeof targetElement.matches === 'function') {
-          const closestHoverElement = targetElement.closest(hoverElementSelector)
-          if (closestHoverElement && closestHoverElement instanceof HTMLAnchorElement) {
-              if ( closestHoverElement.href.includes('/live/') ) {
-                const nowHoverLive = closestHoverElement.href.replace('https://chzzk.naver.com/live/', '')
-
-                  const liveInfo = document.querySelector('.navigator_inner__fR6Zm');
-                  if (liveInfo && !liveInfo.querySelector('img')) {
-                    if (getInquiryStatus(nowHoverLive, liveData)) {
-                      const response = await getStreamData(lastHoverLive)
-                      const image = response.content.liveImageUrl.replace('{type}', '480')
-    
-                      const previewImage = document.createElement('img');
-                      previewImage.setAttribute('src', image);
-                      previewImage.setAttribute('alt', 'Image Description');
-                      previewImage.classList.add('previewImage')
-                      previewImage.style.width = '176px'
-    
-                      // 이미지를 부모 엘리먼트에 추가
-                      liveInfo.appendChild(previewImage);
-                    }
-                  }
-              }
-          }
-
-      }
-      */
-
+      const data = container.getDataById(hoverID);
+      infoElementController(hoverID, data.url);
     });
-
-
-    /*
-    // MutationObserver 생성
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        if (mutation.addedNodes.length > 0) {
-          let isLogin = false;
-          const elements = document.querySelectorAll('.navigator_list__cHnuV');
-          const loginElement = document.querySelector('.toolbar_login__xdtjd');
-          const profileElement = document.querySelector('.toolbar_button__ucivH');
-
-          if (!loginElement && profileElement) isLogin = true;
-          if (loginElement && !profileElement) isLogin = false;
-
-          // 가져온 요소 동작 수행
-          if (elements.length >= (isLogin ? 2 : 1)) {
-            // console.log(elements)
-
-            const liveInfo = document.querySelector('.navigator_inner__fR6Zm');
-            if (liveInfo) {
-              // Check if img element is already present
-              if (!liveInfo.querySelector('img')) {
-
-                // let streamerURL = `https://api.chzzk.naver.com/service/v2/channels/${a}/live-detail`
-
-                const previewImage = document.createElement('img');
-                previewImage.setAttribute('src', 'https://livecloud-thumb.akamaized.net/chzzk/livecloud/KR/stream/26479452/live/4271072/record/24773341/thumbnail/image_480.jpg');
-                previewImage.setAttribute('alt', 'Image Description');
-                previewImage.classList.add('previewImage')
-                previewImage.style.width = '176px'
-
-                // 이미지를 부모 엘리먼트에 추가
-                liveInfo.appendChild(previewImage);
-              }
-            }
-          }
-        }
-      });
-    });
-
-    // 대상 노드 설정 (여기서는 document.body, 전체 문서)
-    const targetNode = document.body;
-
-    // 설정한 대상 노드와 감시할 변경 사항 타입 설정
-    const config = { childList: true, subtree: true };
-
-    // MutationObserver 시작
-    observer.observe(targetNode, config);
-
-    // observer 해제 (componentWillUnmount에 해당하는 부분)
-    return () => {
-      observer.disconnect();
-    };
-
-    */
   }, []);
   return null; // 렌더링할 내용이 없는 경우
 }
